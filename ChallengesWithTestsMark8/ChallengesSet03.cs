46d0f506b53ff28a5ed065cc0879a2d5355393bb
Copacetic Cindy <therivermont@gmail.com>
2020-07-27 14:01:42 -0500

    Weekly 03 Exercises complete!

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks.Sources;
 
 namespace ChallengesWithTestsMark8
@@ -21,14 +22,16 @@ namespace ChallengesWithTestsMark8
             //  Still missing the first test for some reason!!!!
             if (numbers == null) return false;
             if (numbers.Count() == 0) return false;
+           
             int sum = 0;
             foreach (int i in numbers)
             {
-                if (i % 2 == 1) sum += i;
+                if ((i % 2 == 1) || (i % 2 == -1)) sum += i;
             }
 
-            if (sum % 2 == 1) return true;
-            return false;
+            if ((sum % 2 == 1) || (sum % 2 == -1)) return true;
+                return false;
+            
         }
 
         public bool PasswordContainsUpperLowerAndNumber(String password)
@@ -66,12 +69,25 @@ namespace ChallengesWithTestsMark8
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            int[] odds = new int[50];
+            int cnt = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    odds[cnt] = i;
+                    cnt++;
+                }
+            }
+            return odds;
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i=0; i< words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
         }
     }
 }
